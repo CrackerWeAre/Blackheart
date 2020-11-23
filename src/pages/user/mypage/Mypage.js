8919ef1c573dcd70d25a3ea86ae2aae33d23e7f9
정윤도 <truenorthj@icloud.com>
2020-11-23 14:03:40 +0900

    feature : 기본 mypage 생성 ##### 제목은 최대 50글자까지만 입력
    # 본문은 위에 작성
    ##### 본문은 한 줄에 최대 72글자까지만 입력
    
    # 연관된 이슈 첨부는 아래에 작성, 여러 개 추가 가능
    ##### EX) # 이슈 번호
    
    # ------------------------------- COMMIT END ------------------------------
    # <타입> 리스트
    # feature : 기능 (새로운 기능)
    # fix       : 버그 (버그 수정)
    # update : 비즈니스 로직 변경
    # docs    : 문서 (문서 추가, 수정, 삭제)
    # test     : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    # etc      : 기타 변경사항
    # -------------------------------------------------------------------------
    # 제목 첫 글자를 대문자로
    # 제목은 명령문으로
    # 제목 끝에 마침표(.) 금지
    # 제목과 본문을 한 줄 띄워 분리하기
    # 본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    # 본문에 여러 줄의 메시지를 작성할 땐 "-" 로 구분
    # -------------------------------------------------------------------------

---
+++ src/pages/user/mypage/Mypage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+export const Mypage = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => ({
+    
+})
+
+const mapDispatchToProps = {
+    
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Mypage)
