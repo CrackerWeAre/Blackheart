b5087239277a25186a71a0a81aca67742397133b
정윤도 <truenorthj@icloud.com>
2020-11-25 01:01:05 +0900

    update : redux connect

---
+++ src/pages/user/mypage/Navibar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+export const Navibar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => ({
+    
+})
+
+const mapDispatchToProps = {
+    
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navibar)
