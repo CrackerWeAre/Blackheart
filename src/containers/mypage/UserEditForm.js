ca25f78a3f62b986234259eb1dbd73741498adec
정윤도 <truenorthj@icloud.com>
2020-12-09 11:53:22 +0900

    Update : mypage css 수정 	- 폰트 및 인포 값 수정

---
+++ src/containers/mypage/UserEditForm.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { changeField, initializeForm, login } from 'modules/auth';
+import AuthForm from 'components/auth/AuthForm';
+
+const UserEditForm = ({ history }) => {
+    const dispatch = useDispatch();
+    const { form, user, userError } = useSelector(({ auth }) => ({
+        form: auth.login,
+        user: auth.user,
+        userError: auth.userError,
+    }));
+    
+    const onChange = e => {
+        const { name, value } = e.target;
+        dispatch(changeField({
+            form: 'login',
+            key: name,
+            value
+        }));
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        const { email, password } = form;
+
+        dispatch(login({ email, password }));
+    }
+
+    useEffect(() => {
+        dispatch(initializeForm('login'));
+    }, [dispatch]);
+
+    return (
+        <AuthForm 
+            type="login"
+            form={form}
+            onChange={onChange}
+            onSubmit={onSubmit}
+        />
+    );
+}
+
+export default withRouter(UserEditForm);
